919041a5e41fe50d3020da5093c152dd5081f884
Vinicius Dutra <vini_6587@hotmail.com>
2020-03-12 21:25:26 -0300

    Projeto criado

---
+++ src/main/java/com/viniciusdutra/mobicell/MobicellApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdutra.mobicell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobicellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobicellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusdutra/mobicell/MobicellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusdutra.mobicell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobicellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
